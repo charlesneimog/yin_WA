8d3815d1045d367b6e0f5e9fddb55bed17ed5468
Charles K. Neimog <charlesneimog@outlook.com>
2023-01-12 19:00:32 -0300

    auto commit

--- src/index.c
+++ src/index.c
@@ -8,15 +8,25 @@
 #endif
 
 // PITCH DETECTION FROM YIN
-float EMSCRIPTEN_KEEPALIVE yinPitchDetector(float *sound, int windowSize, int sampleRate){
-    int buffer_length = windowSize;  
-    Yin yin;
-    float pitch;
+float EMSCRIPTEN_KEEPALIVE C_yinPitchDetector(float sound[], int windowSize, int sampleRate){
+   int buffer_length = windowSize;  
+   Yin yin;
+   float pitch;
+   float newSound[buffer_length];
 
-     while (pitch < 10) {
-        Yin_init(&yin, buffer_length, 0.05);
-        pitch = Yin_getPitch(&yin, sound);
-        buffer_length++;
-    }
-    return pitch;
+   // copy the sound array into a new array
+   for (int i = 0; i < buffer_length; i++) {
+      newSound[i] = sound[i];
+   }
+
+   // initialise the yin object
+   Yin_init(&yin, buffer_length, 0.05);
+
+   // calculate the pitch
+   while (pitch < 10 ) {
+      pitch = Yin_getPitch(&yin, newSound);	
+      buffer_length++;
+   }
+
+   return pitch;
 }
